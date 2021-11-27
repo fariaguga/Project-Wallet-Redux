6d286ec1bd92dda12e2ab8f5a7c00ee9360cfb5b
Gustavo <fariaguga@hotmail.com>
2021-11-27 09:51:31 -0200

    Adiciona Requisito 7

--- src/components/TableExpenses.js
+++ src/components/TableExpenses.js
@@ -1,23 +1,53 @@
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class TableExpenses extends Component {
   render() {
+    const { expenses } = this.props;
     return (
       <div>
         <header>
           <table>
-            <td>
-              <th id="Descrição" value="Descrição">Descrição</th>
-              <th id="" value="">Tag</th>
-              <th id="" value="">Método de pagamento</th>
-              <th id="" value="">Valor</th>
-              <th id="" value="">Moeda</th>
-              <th id="" value="">Câmbio utilizado</th>
-              <th id="" value="">Valor convertido</th>
-              <th id="" value="">Moeda de conversão</th>
-              <th id="" value="">Editar/Excluir</th>
-
-            </td>
+            <tr>
+              <th>Descrição</th>
+              <th>Tag</th>
+              <th>Método de pagamento</th>
+              <th>Valor</th>
+              <th>Moeda</th>
+              <th>Câmbio utilizado</th>
+              <th>Valor convertido</th>
+              <th>Moeda de conversão</th>
+              <th>Editar/Excluir</th>
+            </tr>
+            <tbody>
+              {
+                expenses.map((elemento) => {
+                  const { id, description, tag, method, value } = elemento;
+                  const findCurrency = Object.entries(elemento.exchangeRates).find(
+                    (exactCurrency) => elemento.currency === exactCurrency[0],
+                  );
+                  const curr = findCurrency[1].name.replace('/Real Brasileiro', '');
+                  return (
+                    <tr key={ id } className="table">
+                      <td>{description}</td>
+                      <td>{tag}</td>
+                      <td>{method}</td>
+                      <td>{value}</td>
+                      <td>{curr}</td>
+                      <td>
+                        {Number(findCurrency[1].ask).toFixed(2)}
+                      </td>
+                      <td>{Number(findCurrency[1].ask * value).toFixed(2)}</td>
+                      <td>Real</td>
+                      <td>Editar/Excluir</td>
+                      { // refetencia : https://github.com/tryber/sd-015-b-project-trybewallet/pull/83/commits/b0fbb563cb1bd090255b102a23e842ac626838fe
+                      }
+                    </tr>
+                  );
+                })
+              }
+            </tbody>
           </table>
         </header>
       </div>
@@ -25,4 +55,14 @@ class TableExpenses extends Component {
   }
 }
 
-export default TableExpenses;
+TableExpenses.propTypes = {
+  expenses: PropTypes.shape({
+    map: PropTypes.func.isRequired,
+  }).isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  expenses: state.wallet.expenses,
+});
+
+export default connect(mapStateToProps, null)(TableExpenses);
